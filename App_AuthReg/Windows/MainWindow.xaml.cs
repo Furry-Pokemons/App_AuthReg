a29d1b8a55187f39a703e78640bb45166de87669
Иван Теплов <TEPL0V@outlook.com>
2022-11-03 10:07:49 +0300

    UPD_n1: -Сделал регистрацию; -Леше надо починить его сломанную авторизацию

---
+++ App_AuthReg/Windows/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using App_AuthReg.Models;
+using App_AuthReg.Windows;
+
+namespace App_AuthReg
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Auth_Click(object sender, RoutedEventArgs e)
+        {
+            IEnumerable<int> Auth = AppData.Context.Database.SqlQuery<int>($"select [dbo].[Auth]('{Login_TB.Text}', '{Password_TB.Password}') as Info");
+            int Auth_Result = int.Parse(Auth.ToList()[0].ToString());
+            if (Auth_Result == 1)
+            {
+                WorkPlace workPlace = new WorkPlace();
+                workPlace.Show();
+                this.Close();
+            }
+        }
+
+        private void Reg_Click(object sender, RoutedEventArgs e)
+        {
+            Registration registration = new Registration();
+            registration.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ App_AuthReg/Windows/Registration.xaml.cs
@@ -0,0 +1,39 @@
+﻿using App_AuthReg.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace App_AuthReg.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void Reg_Click(object sender, RoutedEventArgs e)
+        {
+            AppData.Context.RegUser(Login_TB.Text, Password_TB.Password);
+            AppData.Context.SaveChanges();
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+
+        }
+    }
+}
